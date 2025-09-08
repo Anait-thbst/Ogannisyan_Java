24ae309340e8eccb73b30dbf9c7722de4850846f
Anait-thbst <anahit.ogannisyan.05@mail.ru>
2025-09-09 02:19:43 +0300

    Changes seminar1.java

--- seminar1.java
+++ seminar1.java
@@ -1,5 +1,5 @@
 public class seminar1 {
     public static void main(String[] args) throws Exception {
-         System.out.println("My name is Anahit");
+         System.out.println("Hello world!!");
     }
 }
